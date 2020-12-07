45fb859ff6ed4c4c949f1d8a55b6afdee76dce79
Андрій <64520200+Andrewsabatyuk@users.noreply.github.com>
2020-12-07 16:36:22 +0200

    add log form

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import Login from './Login'
+import React, { useState } from "react";
+import Login from "./Login";
 
 function App() {
-  return <Login/>
+  const [id, setId] = useState();
+  return (
+    <>
+      {id}
+      <Login onIdSubmit={setId} />
+    </>
+  );
 }
 
 export default App;
