81120474e6ca7ff8c930f6b4d7542934ea61698c
Андрій <64520200+Andrewsabatyuk@users.noreply.github.com>
2021-01-28 10:41:42 +0200

    final fixes

--- chat-app/client/src/contexts/SocketProvider.js
+++ chat-app/client/src/contexts/SocketProvider.js
@@ -5,25 +5,25 @@ const SocketContext = React.createContext()
 
 
 export function useSocket(){
-    return useContext(SocketContext)
+	return useContext(SocketContext)
 }
 
 export function SocketProvider({id, children}) {
-    const [socket, setSocket] = useState()
+	const [socket, setSocket] = useState()
 
-    useEffect(()=>{
-        const newSocket = io(
-            'http://localhost:5000',
-         {query:{id}}
-         )
-         setSocket(newSocket)
+	useEffect(()=>{
+		const newSocket = io(
+			'http://localhost:5000',
+		{query:{id}}
+		)
+		setSocket(newSocket)
 
-         return() => newSocket.close()
-    }, [id])
+		return() => newSocket.close()
+	}, [id])
 
-    return (
-        <SocketContext.Provider value={socket}>
-            {children}
-        </SocketContext.Provider>
-    )
+	return (
+		<SocketContext.Provider value={socket}>
+			{children}
+		</SocketContext.Provider>
+	)
 }
